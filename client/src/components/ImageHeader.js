5ebdb0dccf12bbcb25e77fa38c9e1394c1d6b0b3
mealthebear <mealthebear@gmail.com>
2020-10-26 14:50:17 -0700

    Placed header title and contact button on page

--- client/src/components/ImageHeader.js
+++ client/src/components/ImageHeader.js
@@ -3,7 +3,14 @@ import React, { useState } from 'react';
 const ImageHeader = () => {
   return (
     <>
-      <div>
+      <div className="header-container">
+        <div className="title-container">
+          <h2 className="header-title">Dennis Chatkhan</h2>
+          <button className="contact-button">Contact</button>
+        </div>
+
+        
+
         <img
           className="bear-image"
           src="/client/dist/images/sitting_bear.jpg"
